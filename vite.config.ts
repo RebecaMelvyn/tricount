37ba26e7e4206b9d23b7e50d56baec5b15551d97
Melvyn <melvyn.rebeca1@gmail.com>
2024-02-05 14:10:48 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+ 
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }, 
+      workbox: {
+        navigateFallback: '/index.html',
+        globPatterns: ['**/*.{js,css,html,png,svg}'],
+      },
+      manifest: {
+        name: 'PWA App',
+        short_name: 'PWA App',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+        ],
+      }
+    })
+  ],
+})
