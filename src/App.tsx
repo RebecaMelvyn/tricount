3f1962e3f81f91895ae5e978d57784165ad202a4
Melvyn <melvyn.rebeca1@gmail.com>
2024-02-06 11:31:14 +0100

    feat: join group, back button, styles pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CreateGroupForm from './components/CreateGroupForm';
 import GroupDetails from './components/GroupDetails';
 import PageAccueil from './components/Accueil';
+import JoinGroupForm from './components/JoinGroup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PageAccueil />} />          
         <Route path="/create-group" element={<CreateGroupForm />} />
+        <Route path="/join-group" element={<JoinGroupForm />} />
         <Route path="/group-details/:groupId" element={<GroupDetails />} />
       </Routes>
     </Router>
